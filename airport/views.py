36c1971882257036bb4718d23c1c327d628a0b6f
user <graban2253@gmail.com>
2025-01-29 16:46:58 +0200

    Added basic views

--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
 
-# Create your views here.
+
+from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from rest_framework.viewsets import GenericViewSet
+
+from airport.models import Airport, Route, AirplaneType, Airplane, Crew, Flight
+from airport.serializers import AirportSerializer, RouteSerializer, AirplaneTypeSerializer, AirplaneSerializer, \
+    CrewSerializer, FlightSerializer
+
+
+class AirportViewSet(mixins.CreateModelMixin,
+                     mixins.ListModelMixin,
+                     GenericViewSet):
+    queryset = Airport.objects.all()
+    serializer_class = AirportSerializer
+
+
+class RouteViewSet(mixins.CreateModelMixin,
+                   mixins.ListModelMixin,
+                   GenericViewSet):
+    queryset = Route.objects.all()
+    serializer_class = RouteSerializer
+
+
+class AirplaneTypeViewSet(mixins.CreateModelMixin,
+                          mixins.ListModelMixin,
+                          GenericViewSet):
+    queryset = AirplaneType.objects.all()
+    serializer_class = AirplaneTypeSerializer
+
+
+class AirplaneViewSet(mixins.CreateModelMixin,
+                      mixins.ListModelMixin,
+                      GenericViewSet):
+    queryset = Airplane.objects.all()
+    serializer_class = AirplaneSerializer
+
+
+class CrewViewSet(mixins.CreateModelMixin,
+                  mixins.ListModelMixin,
+                  GenericViewSet):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
+
+
+class FlightViewSet(mixins.CreateModelMixin,
+                    mixins.ListModelMixin,
+                    GenericViewSet):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
