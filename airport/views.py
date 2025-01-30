3e28eaa7f7018023d2fbdc0ece59c70fe11c84c8
user <graban2253@gmail.com>
2025-01-30 04:12:22 +0200

    Added permissions

--- airport/views.py
+++ airport/views.py
@@ -1,68 +1,77 @@
-
-
 from django.shortcuts import render
 from rest_framework import viewsets, mixins
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.viewsets import GenericViewSet
 
 from airport.models import Airport, Route, AirplaneType, Airplane, Crew, Flight, Order
-from airport.serializers import AirportSerializer, RouteSerializer, AirplaneTypeSerializer, AirplaneSerializer, \
-    CrewSerializer, FlightSerializer, OrderSerializer, OrderListSerializer
+from airport.permissions import IsAdminOrIfAuthenticatedReadOnly
+from airport.serializers import (
+    AirportSerializer,
+    RouteSerializer,
+    AirplaneTypeSerializer,
+    AirplaneSerializer,
+    CrewSerializer,
+    FlightSerializer,
+    OrderSerializer,
+    OrderListSerializer,
+)
 
 
-class AirportViewSet(mixins.CreateModelMixin,
-                     mixins.ListModelMixin,
-                     GenericViewSet):
+class AirportViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, GenericViewSet):
     queryset = Airport.objects.all()
     serializer_class = AirportSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
-class RouteViewSet(mixins.CreateModelMixin,
-                   mixins.ListModelMixin,
-                   GenericViewSet):
+class RouteViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, GenericViewSet):
     queryset = Route.objects.all()
     serializer_class = RouteSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
-class AirplaneTypeViewSet(mixins.CreateModelMixin,
-                          mixins.ListModelMixin,
-                          GenericViewSet):
+class AirplaneTypeViewSet(
+    mixins.CreateModelMixin, mixins.ListModelMixin, GenericViewSet
+):
     queryset = AirplaneType.objects.all()
     serializer_class = AirplaneTypeSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
-class AirplaneViewSet(mixins.CreateModelMixin,
-                      mixins.ListModelMixin,
-                      GenericViewSet):
+class AirplaneViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, GenericViewSet):
     queryset = Airplane.objects.all()
     serializer_class = AirplaneSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
-class CrewViewSet(mixins.CreateModelMixin,
-                  mixins.ListModelMixin,
-                  GenericViewSet):
+class CrewViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, GenericViewSet):
     queryset = Crew.objects.all()
     serializer_class = CrewSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
-class FlightViewSet(mixins.CreateModelMixin,
-                    mixins.ListModelMixin,
-                    GenericViewSet):
+class FlightViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, GenericViewSet):
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
-class OrderViewSet(mixins.CreateModelMixin,
-                   mixins.ListModelMixin,
-                   GenericViewSet):
-    queryset = Order.objects.prefetch_related(
-        "tickets", "tickets__flight"
-    )
+class OrderViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, GenericViewSet):
+    queryset = Order.objects.prefetch_related("tickets", "tickets__flight")
     serializer_class = OrderSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
     def get_queryset(self):
         return Order.objects.filter(user=self.request.user)
 
     def get_serializer_class(self):
-        if self.action == 'list':
+        if self.action == "list":
             return OrderListSerializer
         return OrderSerializer
 
